fff9692c09f430fc8fc0ef1cbf5a7aeeeee33213
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-07-10 12:50:26 -0600

    Call player join event in plugin

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-use wasmtime::{Store, Module, Extern, Func, Trap, Caller, Linker, Engine, Config};
+use wasmtime::{Store, Module, Extern, Func, Trap, Caller, Linker, Engine, Config, Val};
 
 use harlequinn::{Certificate, EndpointEvent, HqEndpoint, PrivateKey};
 
@@ -33,19 +33,43 @@ fn main() {
         Ok(())
     }).unwrap();
 
-    linker.func("env", "register_player_join_listener", |plugin: i32, callback: i32| {
+    let player_join_listener: std::rc::Rc<std::cell::RefCell<Option<u32>>> = std::rc::Rc::new(std::cell::RefCell::new(None));
+
+    let player_join_listener_clone = std::rc::Rc::clone(&player_join_listener);
+    linker.func("env", "register_player_join_listener", move |plugin: i32, callback: i32| {
         println!("register_player_join_listener({}, {})", plugin as u32, callback as u32);
+        let mut listener = player_join_listener_clone.borrow_mut();
+        listener.replace(callback as u32);
         Ok(())
     }).unwrap();
 
-    linker.func("env", "player_name", |player: i32, plugin: i32, ptr_out: i32, len_out: i32| {
+    linker.func("env", "player_name", |caller: Caller<'_>, player: i32, plugin: i32, ptr_out: i32, len_out: i32| {
         println!("player_name({}, {}, {}, {})", player as u32, plugin as u32, ptr_out as u32, len_out as u32);
+        let mem = match caller.get_export("memory") {
+            Some(Extern::Memory(mem)) => mem,
+            _ => return Err(Trap::new("failed to find host memory")),
+        };
+        unsafe {
+            let data = mem.data_unchecked();
+            let ptr_out_data = match data.get(ptr_out as u32 as usize..).map(|arr| arr.as_ptr() as *mut u32) {
+                Some(d) => d,
+                None => return Err(Trap::new("pointr out pointer out of bounds")),
+            };
+            let len_out_data = match data.get(len_out as u32 as usize..).map(|arr| arr.as_ptr() as *mut u32) {
+                Some(d) => d,
+                None => return Err(Trap::new("length out pointer out of bounds")),
+            };
+
+            *ptr_out_data = 1;
+            *len_out_data = 0;
+        }
         Ok(())
     }).unwrap();
 
     let instance = linker.instantiate(&module).unwrap();
 
     let on_enable = instance.get_func("on_enable").ok_or(anyhow::format_err!("failed to find `on_enable` function export")).unwrap().get1::<i32, ()>().unwrap();
+    let plugin_callback_table = instance.get_table("__indirect_function_table").ok_or(anyhow::format_err!("failed to find `__indirect_function_table` export")).unwrap();
 
     on_enable(1234).unwrap();
 
@@ -69,6 +93,16 @@ fn main() {
                 } => {
                     endpoint.accept(peer_id);
                     println!("Client connected: {}", socket_addr);
+
+                    let listener_opt = player_join_listener.borrow()
+                        .map(|callback| plugin_callback_table.get(callback))
+                        .flatten()
+                        .map(|val| match val {Val::FuncRef(func) => Some(func), _ => None})
+                        .flatten();
+                    if let Some(listener_func) = listener_opt {
+                        let listener = listener_func.get4::<i32,i32,i32,i32,()>().unwrap();
+                        listener(1234, 1, 1, 1).unwrap();
+                    }
                 }
                 EndpointEvent::Disconnected { .. } => {
                     println!("Client disconnected");
